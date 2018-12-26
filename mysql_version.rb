d2fb066750c927e07907ff959257b52d6887eaf7
anoop narang <anoop@go-jek.com>
2018-12-26 14:41:40 +0530

    Add mysql password

--- mysql_version.rb
+++ mysql_version.rb
@@ -1,7 +1,7 @@
 require 'mysql2'
 require 'pp'
 
-client = Mysql2::Client.new(:host => 'localhost', :username => ENV['MYSQL_USER'])
+client = Mysql2::Client.new(:host => 'localhost', :username => ENV['MYSQL_USER'], :password => ENV['MYSQL_PASSWORD'])
 client.query("SELECT version()").each do |row|
   pp row
 end
